e7e73a2a48e974131e31ccdbbae19d3a6988e2fd
emmacps <emmacps001@gmail.com>
2023-05-21 12:51:31 +0000

    frontend template setup

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    return view('frontend.index');
 });
 
 Route::get('/dashboard', function () {
