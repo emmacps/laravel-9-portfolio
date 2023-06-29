86942128c3228d6717538da5097e613c79388707
emmacps <emmacps001@gmail.com>
2023-06-29 13:03:06 +0000

    complete footer section

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -81,6 +81,17 @@
                 <li>
                     <a href="javascript: void(0);" class="has-arrow waves-effect">
                         <i class="ri-profile-line"></i>
+                        <span>Footer Setup</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="{{ route('footer.setup') }}">All Blog</a></li>
+                        
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="ri-profile-line"></i>
                         <span>Blog Post</span>
                     </a>
                     <ul class="sub-menu" aria-expanded="false">
